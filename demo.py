b62733a859fd9ada6a2efc12a0347d33aa5d8637
laiwola <layiibrahim32@gmail.com>
2025-10-08 17:04:48 +0100

    first-commit

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
 from Block_intel_classifier.logger import logging
+from Block_intel_classifier.exception import Block_intelException
+import sys
 logging.info("Testing my logging file")
+try:
+    x=3/0
+except Exception as e:
+   raise Block_intelException(e,sys)
